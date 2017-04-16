01cc0e223c373ce7389f3ee86389d0ef81750759
heissi <heissi@tlen.pl>
2017-04-16 11:15:38 +0200

    Kamil

--- heissi/main.cpp
+++ heissi/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 using namespace std;
+class pole
+{
+public:
+	enum stan
+	{
+		kolko = 0,
+		krzyzyk = 1,
+		puste = 3
+	};
+	stan stan_pola;
+	pole()
+	{
+		stan_pola = stan.puste;
+	}
+};
 int main()
 {
 	cout << "Hello World" << endl;
