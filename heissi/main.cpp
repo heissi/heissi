54a441f70b1ec475c090f9d86f8a76e0f27d6cb2
heissi <heissi@tlen.pl>
2017-04-16 14:32:30 +0200

    Plansza

--- heissi/main.cpp
+++ heissi/main.cpp
@@ -8,7 +8,7 @@ public:
 	{
 		kolko = 0,
 		krzyzyk = 1,
-		puste = 3
+		puste = 2
 	};
 	stan stan_pola;
 	pole()
@@ -16,8 +16,29 @@ public:
 		stan_pola = stan::puste;
 	}
 };
+pole plansza[3][3];
 int main()
 {
+	for (int y=0;y<3;y++)
+	{
+		for (int x=0;x<3;x++)
+		{
+			if (plansza[x][y].stan_pola == pole::stan::puste)
+			{
+				cout << " ";
+			}
+			else if (plansza[x][y].stan_pola == pole::stan::kolko)
+			{
+				cout <<"O";
+			}
+			else
+			{
+				cout << "X";
+			}
+		}
+		cout << endl;
+	}
+
 	cout << "Hello World" << endl;
 	cin.get();
 	return 0;
